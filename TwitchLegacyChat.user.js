3ff787c42a100d6f6084e3b8017baf5ac9df0e99
unclear-asdf <theandrewhoward@gmail.com>
2018-02-08 01:42:02 -0900

    Added @run-at document-start, removed /*/chat from excludes

--- TwitchLegacyChat.user.js
+++ TwitchLegacyChat.user.js
@@ -1,18 +1,17 @@
 // ==UserScript==
 // @name         Twitch Legacy Chat
-// @version      0.6.0a
+// @version      0.6.0b
 // @description  Replaces the regular, new Twitch chat with the Legacy Popout in an iframe. Restores the functionality of FrankerFaceZ and BTTV. Based on this Chrome extension: https://chrome.google.com/webstore/detail/twitch-legacy-chat/nonbdfmekapigdafeajhmhbkbnfibfhg
 // @grant        none
 // @include https://*.twitch.tv/*
 // @include http://*.twitch.tv/*
 // @include https://twitch.tv/*
 // @include http://twitch.tv/*
-// @exclude https://twitch.tv/*/chat
-// @exclude http://twitch.tv/*/chat
 // @exclude http://api.twitch.tv/*
 // @exclude https://api.twitch.tv/*
 // @require http://code.jquery.com/jquery-latest.js
 // @require http://code.jquery.com/ui/1.12.0/jquery-ui.min.js
+// @run-at       document-start
 // ==/UserScript==
 $(window).load(function () {
 
